44a58e94f8e9f71f500e5a0a1cb9355e5925956c
Aaron Dettmann <dettmann@kth.se>
2019-08-22 09:15:08 +0200

    Added basic test for 'math.interpolation'

--- docs/TODO.md
+++ docs/TODO.md
@@ -6,6 +6,11 @@
     * Make `join_path()` function more general (any number of paths)
     * Separate `join_path()` function outside of `ProjectPaths()`
 
+## math.interpolation
+
+* `lin_interpol()`
+    * Change order: x12, y12
+
 ## Documentation
 * Add documentation
 
--- tests/math/test_interpolation.py
+++ tests/math/test_interpolation.py
@@ -4,5 +4,15 @@
 import commonlibs.math.interpolation as m
 
 
-def test_basic():
-    pass
+def test_lin_interpol():
+    """
+    Test function for linear interpolation
+    """
+
+    x12 = (0, 10)
+    y12 = (20, 40)
+    assert m.lin_interpol(y12=y12, x12=x12, x=5) == 30
+
+    x12 = (0, 1)
+    y12 = (0, 1)
+    assert m.lin_interpol(y12=y12, x12=x12, x=0.5) == 0.5
