12c98b0bcfbe51cfbb2d7b07f72259edd865d6df
GBenedett <113425078+GBenedett@users.noreply.github.com>
2025-05-08 13:53:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,19 +22,13 @@ README = 'README.rst'
 PACKAGE_DIR = 'src/lib/'
 LICENSE = 'Apache License 2.0'
 
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, README), "r") as fp:
-    long_description = fp.read()
-
 setup(
     name=NAME,
     version=VERSION,
     author=AUTHOR,
     author_email=EMAIL,
     description=DESCRIPTION,
-    long_description=long_description,
+    long_description=open("README.md").read(),
     url=URL,
     include_package_data=True,
     package_dir={'': PACKAGE_DIR},
